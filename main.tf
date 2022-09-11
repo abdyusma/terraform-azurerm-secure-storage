110e25883274c5bdeb8792352d1908baf6d30225
abdyusma <abdul.rahman.yusmadi@crayon.com>
2022-09-11 11:26:21 +0800

    Initial Terraform configurations

---
+++ main.tf
@@ -0,0 +1,13 @@
+data "azurerm_resource_group" "existing_resource_group" {
+  name = var.existing_resource_group_name
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = var.storage_account_name
+  resource_group_name      = data.azurerm_resource_group.existing_resource_group.name
+  location                 = data.azurerm_resource_group.existing_resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  public_network_access_enabled = false
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "existing_resource_group_name" {
+  description = "Existing resource group to hosts the Azure storage account."
+}
+
+variable "storage_account_name" {
+    description = "Unique all-lowercase value with no space"
+}
