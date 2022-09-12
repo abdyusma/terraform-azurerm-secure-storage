bcbb40b75c848bd13ef40e4ac06652e5d737fac1
abdyusma <111057405+abdyusma@users.noreply.github.com>
2022-09-13 01:40:19 +0800

    Add allowed_ip variable

--- variables.tf
+++ variables.tf
@@ -5,3 +5,8 @@ variable "existing_resource_group_name" {
 variable "storage_account_name" {
   description = "Unique all-lowercase value with no space"
 }
+
+variable "allowed_ip" {
+  description = "List of allowed public IP or IP ranges in CIDR Format."
+  type        = list
+}
