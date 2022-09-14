377d9c951d5a0b4144c06e947816bed92302c10e
Abdul Rahman Yusmadi <111057405+abdyusma@users.noreply.github.com>
2022-09-15 00:19:55 +0800

    Update main.tf
    Make allowed IP addresses available.

--- main.tf
+++ main.tf
@@ -3,20 +3,18 @@ data "azurerm_resource_group" "existing_resource_group" {
 }
 
 resource "azurerm_storage_account" "main" {
-  name                     = var.storage_account_name
-  resource_group_name      = data.azurerm_resource_group.existing_resource_group.name
-  location                 = data.azurerm_resource_group.existing_resource_group.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  public_network_access_enabled = false
-  shared_access_key_enabled     = false
+  name                      = var.storage_account_name
+  resource_group_name       = data.azurerm_resource_group.existing_resource_group.name
+  location                  = data.azurerm_resource_group.existing_resource_group.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  shared_access_key_enabled = false
 }
 
-resource "azurerm_storage_account_network_rules" "github_actions" {
-  storage_account_id = azurerm_storage_account.main.id
-
-  default_action = "Allow"
-  ip_rules       = var.allowed_ip
-  bypass         = ["Metrics"]
+resource "azurerm_storage_account_network_rules" "main" {
+  storage_account_id         = azurerm_storage_account.main.id
+  default_action             = "Deny"
+  ip_rules                   = var.allowed_ip
+  virtual_network_subnet_ids = []
+  bypass                     = ["Metrics"]
 }
